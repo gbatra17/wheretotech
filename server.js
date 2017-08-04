b0dd01b7be103d40bab64bcd352d31e47cd2a589
gbatra17 <gbatra17@gmail.com>
2017-08-04 15:53:35 -0400

    added basic server text

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 var express = require('express');
+var partials = require('express-partials');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 
 var app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({encoded: true }));
 app.get('/', function(req, res) {
-	res.render('hello world!');
+	res.render('<h1> hello world! </h1>');
 })
