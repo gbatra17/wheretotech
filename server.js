f722f35eb41a3857239e4cad7b2d06e08f128f4a
gbatra17 <gbatra17@gmail.com>
2017-08-04 16:45:12 -0400

    rendering title on local host

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var partials = require('express-partials');
 var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
 
 var app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({encoded: true }));
+app.use(express.static(__dirname + '/client'));
 app.get('/', function(req, res) {
-	res.send('<h1> hello world! </h1>');
+	res.sendFile('index.html');
+})
+
+app.listen(3000, function() {
+	console.log('MVC listening on port 3000!');
 })
