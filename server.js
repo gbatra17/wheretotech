e1abc4a5ea94f5e54ae0c75f8def6854d7f7d776
gbatra17 <gbatra17@gmail.com>
2017-08-04 18:05:30 -0400

    removed script.js file src

--- server.js
+++ server.js
@@ -11,6 +11,6 @@ app.get('/', function(req, res) {
 	res.sendFile('index.html');
 })
 
-app.listen(3000, function() {
-	console.log('MVC listening on port 3000!');
-})
+// app.listen(3000, function() {
+// 	console.log('MVC listening on port 3000!');
+// })
