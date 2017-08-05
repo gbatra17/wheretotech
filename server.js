964a9910b732b5e3c8a2ecc2b7aa1b1a74ded360
gbatra17 <gbatra17@gmail.com>
2017-08-04 22:39:35 -0400

    added glyphicon library and stubbed out search

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
+//modules ====================================
 var express = require('express');
 var partials = require('express-partials');
 var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 
 var port = process.env.PORT || '3000';
 var app = express();
+var db = require('./config/db.js')
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride('X-HTTP-Method-Override')); 
 app.use(express.static(__dirname + '/client'));
-app.get('/', function(req, res) {
-	res.sendFile('index.html');
-});
 
 app.listen(port, function() {
 	console.log('MVC listening on port 3000!');
