f5dcaf7480f1f872f1a923ef253f1cb12c23b3b9
gbatra17 <gbatra17@gmail.com>
2017-08-07 02:18:06 -0400

    Added db connection

---
+++ server/config.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+
+var uri = 'mongodb://heroku_sw1rhwdk:aat5h7pb7gev9h0okb4cti9hj2@ds051640.mlab.com:51640/heroku_sw1rhwdk'
+
+mongoose.Promise = global.Promise
+
+mongoose.connect(uri);
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', function () {
+  console.log('Mongodb connection open');
+});
+
+module.exports = db;
