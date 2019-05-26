a078f431e5ad9b66491fcb0c36acbdf5a08a27cd
gbatra17 <gbatra17@gmail.com>
2019-05-26 12:40:39 -0400

    Add styled components and input component

---
+++ client2/src/pages/_index.elements.js
@@ -0,0 +1,38 @@
+import styled from "styled-components"
+
+export const Input = styled.input`
+  border: none;
+  border-bottom: 5px solid white;
+  background: transparent;
+  font-size: 80px;
+  height: 80px;
+  width: 300px;
+  color: white;
+  outline: none;
+  padding: 60px 0px 10px 0px;
+  cursor: pointer;
+  text-align: center;
+  font-family: "Roboto", arial, sans-serif;
+  font-weight: bold;
+  &:hover,
+  &:active {
+    outline: none;
+  }
+  @media (max-width: 1030px) {
+    font-size: 55px;
+    height: 55px;
+  }
+  @media (max-width: 435px) {
+    font-size: 40px;
+    height: 40px;
+    margin-top: 20px;
+  }
+  @media (max-height: 360px) {
+    font-size: 30px;
+  }
+  @media (max-width: 360px) {
+    font-size: 30px;
+    height: 35px;
+    margin-top: 10px;
+  }
+`
--- client2/src/pages/index.js
+++ client2/src/pages/index.js
@@ -3,12 +3,16 @@ import Typist from "react-typist"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { Input } from "./_index.elements"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <Typist className="TypistExample-header">
-      <span> > where do you want to : code?</span>
+    <Typist
+      className="TypistExample-header"
+      cursor={{ hideWhenDone: true, hideWhenDoneDelay: 0 }}
+    >
+      <span> > where do you want to code?</span>
       <Typist.Backspace count={5} delay={200} />
       <span> do UX?</span>
       <Typist.Backspace count={6} delay={200} />
@@ -16,6 +20,7 @@ const IndexPage = () => (
       <Typist.Backspace count={25} delay={200} />
       <span>tech? </span>
     </Typist>
+    <Input />
   </Layout>
 )
 
